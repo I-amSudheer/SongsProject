d722ab911372f67f8946450373aa3d3bc3d7f07c
sudheer944066 <yalmarthisivasaisudheer@gmail.com>
2026-05-31 15:51:34 +0530

    Updated backend for Neon database

--- backend/src/main/java/backend/repository/UserRepository.java
+++ backend/src/main/java/backend/repository/UserRepository.java
@@ -2,8 +2,8 @@ package backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import backend.entity.User;
+import backend.entity.Users;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
